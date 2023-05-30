0e8d5ec3ff509833dd1fe880412c5906bad49aa7
Tusharpankhaniya <pankhaniyatushar9@gmail.com>
2023-05-30 11:31:57 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,20 +27,17 @@ const Navbar = () => {
     //         });
     // }
 
-    useEffect(() => {
-        SearchBar();
-    },[searchbar])
+    // useEffect(() => {
+    //     SearchBar();
+    // },[searchbar])
 
     return (
         <>
         <nav className='bg-gray-300/60 flex justify-center p-4 gap-2'>
-
             <input type="text" name="search" className='w-[422px] p-2 outline-none rounded-lg border-2 border-black/30' placeholder='What are you looking for' onChange={(e) => setSearchbar(e.target.value)}/>
 
             <button className="bg-green-500 px-5 text-white flex gap-2 items-center font-semibold rounded-lg"><AiOutlineSearch size={20} /> Search</button>
-
         </nav>
-
         </>
     )
 }
